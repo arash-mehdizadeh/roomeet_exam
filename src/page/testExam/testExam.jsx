191b0f7e2e9371e77f299999ea02e125af952022
Arash <arash.mehdiizadeh@gmail.com>
2022-10-25 14:27:29 +0330

    changed style of containers to grid and add sample countdown hook

--- src/page/testExam/testExam.jsx
+++ src/page/testExam/testExam.jsx
@@ -4,28 +4,35 @@ import TestQuestion from '../../components/testQuestion';
 import { ReactComponent as Exit } from '../../assets/icons/exit.svg';
 import { ReactComponent as Refresh } from '../../assets/icons/RightSquare.svg';
 
-import classes from'../../App.module.scss';
+import classes from '../../App.module.scss';
+import { useCountdown } from '../../components/countDown/useCountDown';
 
 function TestExam() {
+
     return (
         <div className={classes.appContainer}>
             <div className={classes.container}>
-                <header className={classes.examTimeRemainedContainer}>
+                <header className={classes.timeRemainedContainer} style={{display:'grid'}}>
                     <div className={classes.headerBox}>
                         <div className={classes.buttonContainer}>
                             <p>اتمام آزمون</p>
                             <p  >ترک آزمون</p>
                         </div>
-                        <svg id={classes.circle_container} viewBox="2 -2 28 36" xmlns="http://www.w3.org/2000/svg">
-                            <linearGradient id={"gradient"}>
-                                <stop id={classes.stop1} offset="0%" />
-                                <stop id={classes.stop2} offset="100%" />
-                            </linearGradient>
-                            <circle id={classes.circle_container__background} r="16" cx="16" cy="16" shape-rendering="geometricPrecision"></circle>
-                            <circle id={classes.circle_container__progress} r="16" cx="16" cy="16" shape-rendering="geometricPrecision">
-                            </circle>
-                        </svg>
-                        {/* <div className='time-remained'>4:20:00</div> */}
+                        <div className={classes.countdownContainer}>
+                            <svg id={classes.circle_container} viewBox="2 -2 28 36" xmlns="http://www.w3.org/2000/svg">
+                                <linearGradient id={"gradient"}>
+                                    <stop id={classes.stop1} offset="0%" />
+                                    <stop id={classes.stop2} offset="100%" />
+                                </linearGradient>
+                                <circle id={classes.circle_container__background} r="16" cx="16" cy="16" shape-rendering="geometricPrecision"></circle>
+                                <circle id={classes.circle_container__progress} r="16" cx="16" cy="16" shape-rendering="geometricPrecision">
+                                </circle>
+                            </svg>
+                            <div className={classes.timeRemained}>
+                                <div id={classes.time}>4:20:00</div>
+                                <div>مانده</div>
+                            </div>
+                        </div>
                     </div>
                     <div className={classes.informationBar}>
                         <div className={classes.examDetails}>
